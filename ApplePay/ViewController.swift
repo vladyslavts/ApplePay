201a51b5f8d1c5d11fe07956cff4f8180633eadb
Vladyslav <>
2020-02-10 10:39:49 +0200

    Initial

---
+++ ApplePay/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ApplePay
+//
+//  Created by Vladyslav Tsykhmistro on 10.02.2020.
+//  Copyright © 2020 Vladyslav Tsykhmistro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
